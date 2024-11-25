e999cdbe51a24a20872a4d2d26bfb359cba1b60b
NagaevRustam <nagaevrustam@yandex.ru>
2024-11-25 15:25:57 +0300

    Change file

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+#!/usr/bin/env php
+
 <?php
+echo "Введите имя" . PHP_EOL;
+$a = trim(fgets(STDIN));
+echo "Введите фамилию" . PHP_EOL;
+$b = trim(fgets(STDIN));
+// echo "Введите отчество" . PHP_EOL;
+// $c = trim(fgets(STDIN));
+$pos = mb_strpos($a, $b);
+//$str = str_replace("р", "Р", "рустам");
+var_dump($pos) . PHP_EOL;
+
+//var_dump(mb_strpos($a, 'руст'));
+
+
+$fullName = fwrite(STDOUT, "$a") . PHP_EOL;
+// echo "$fullName";
+
+//$fio = 'ИИИ';
+//$surnameAndInitials = 'Иванов И.И.';
 
 ?>
